93b81cc2d9ce4957d7ac2c47c97ce40f2e44dd66
Kohei Asai <me@axross.io>
2019-08-26 16:04:09 -0700

    About Me

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Kohei Asai on 8/26/19.
+//  Copyright © 2019 Kohei Asai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
